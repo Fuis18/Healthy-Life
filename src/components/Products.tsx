bcb14b9993430a05b72ae329ca1424fb82d16211
Fuis18 <96286214+Fuis18@users.noreply.github.com>
2025-06-20 05:45:41 -0500

    Add files via upload

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -6,7 +6,7 @@ const Products = () => {
 			<Product
 				title="Brownie de Plátano"
 				foto="/image(1).webp"
-				link="/FICHA_TECNICA_BROWNIE_DE_PLATANO.webp"
+				link="/FICHA-TECNICA-BROWNIE-DE-PLATANO.webp"
 			>
 				Dulzura natural y textura esponjosa. El sabor del plátano se
 				mezcla con el cacao y la palta para crear un brownie delicioso y
@@ -15,7 +15,7 @@ const Products = () => {
 			<Product
 				title="Brownie de Frutos Rojos"
 				foto="/image(2).webp"
-				link="/FICHA TECNICA BROWNIE DE FRUTOS ROJOS.webp"
+				link="/FICHA-TECNICA-BROWNIE-DE-FRUTOS-ROJOS.webp"
 			>
 				Una fusión deliciosa entre lo dulce y lo ácido. Brownie de palta
 				con un toque de frutos rojos que aporta frescura, antioxidantes
@@ -24,7 +24,7 @@ const Products = () => {
 			<Product
 				title="Brownie de Frutos Secos"
 				foto="/image(3).webp"
-				link="/FICHA TECNICA BROWNIE DE FRUTOS SECOS.webp"
+				link="/FICHA-TECNICA-BROWNIE-DE-FRUTOS-SECOS.webp"
 			>
 				Nuestro brownie de palta con frutos secos combina el cacao con
 				nueces, almendras y más, para darte energía saludable en cada
