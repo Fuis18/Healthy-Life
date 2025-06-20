52e48e92d89ae1ce0cdf30dd07c92db560cad008
Fuis18 <96286214+Fuis18@users.noreply.github.com>
2025-06-20 04:50:59 -0500

    Add files via upload

--- src/components/Galeria.tsx
+++ src/components/Galeria.tsx
@@ -1,5 +1,54 @@
+import "@/styles/galeria.css";
+import Image from "next/image";
 const Galeria = () => {
-	return <div>Galeria</div>;
+	const width = 400;
+	const height = 200;
+	return (
+		<div className="wrapper">
+			<Image
+				src="/packj(1).webp"
+				alt="Healthy Life"
+				width={width}
+				height={height}
+				className="item item1"
+			></Image>
+			<Image
+				src="/packj(2).webp"
+				alt="Healthy Life"
+				width={width}
+				height={height}
+				className="item item2"
+			></Image>
+			<Image
+				src="/packj(3).webp"
+				alt="Healthy Life"
+				width={width}
+				height={height}
+				className="item item3"
+			></Image>
+			<Image
+				src="/packj(4).webp"
+				alt="Healthy Life"
+				width={width}
+				height={height}
+				className="item item4"
+			></Image>
+			<Image
+				src="/packj(5).webp"
+				alt="Healthy Life"
+				width={width}
+				height={height}
+				className="item item5"
+			></Image>
+			<Image
+				src="/packj(6).webp"
+				alt="Healthy Life"
+				width={width}
+				height={height}
+				className="item item6"
+			></Image>
+		</div>
+	);
 };
 
 export default Galeria;
